ae21e5b9db1979bc5ac37dee28b6d1ed74cbd896
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-28 19:22:49 +0530

    ADDED :Welcome to Snake Ladder Simulation

---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake Ladder Simulator");
+		// TODO Auto-generated method stub
+
+	}
+
+}
