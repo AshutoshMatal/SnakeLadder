26745e32586920d520fda86a1ed33258f29b21a3
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-28 19:29:15 +0530

    ADDED : Initailize the start position at zero

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,6 +3,8 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake Ladder Simulator");
+		int position=0;
+		int player;
 		// TODO Auto-generated method stub
 
 	}
