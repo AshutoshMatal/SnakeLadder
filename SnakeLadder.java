b58e148bf712292acfb05a4e6256fb6e2f71ab2a
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-29 10:59:42 +0530

    Modified : Random function to roll the dice

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class SnakeLadder {
 
@@ -5,8 +6,14 @@ public class SnakeLadder {
 		System.out.println("Welcome to Snake Ladder Simulator");
 		int position=0;
 		int player;
+		int dice;
+		dice=(int)((Math.random()*6)+1);
+		System.out.println(dice);	
+		
+		
+		}
 		// TODO Auto-generated method stub
 
 	}
 
-}
+
